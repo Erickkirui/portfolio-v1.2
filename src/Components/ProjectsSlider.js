7ad8752dd51a8308069205abe0ea3265dab3a9d0
Erickkirui <erickkirui653@gmail.com>
2025-01-21 15:57:21 +0300

    updated the slide size

--- src/Components/ProjectsSlider.js
+++ src/Components/ProjectsSlider.js
@@ -9,7 +9,7 @@ import '../Styles/sliderprojects.css';
 function ProjectsSlider() {
   return (
     <div className="slider-wrapper">
-      {/* First Swiper with 4 cards */}
+      {/* First Swiper with 4 cards (Right-to-Left) */}
       <Swiper
         spaceBetween={20}
         slidesPerView={3}
@@ -29,75 +29,78 @@ function ProjectsSlider() {
         </SwiperSlide>
         <SwiperSlide>
           <div className="slider-item">
-            <img src="/images/ALSEYEEE-BIO.jpg" alt="Virgiways Consultants" className="slider-image" />
+            <img src="/images/ALSEYEEE-BIO.jpg" alt="ALSEYEEE Bio" className="slider-image" />
             <div className="text-overlay">
-              <h1>Virgiways Consultants</h1>
+              <h1>ALSEYEEE Bio</h1>
               <button>View website</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slider-item">
-            <img src="/images/dcngo.jpg" alt="Virgiways Consultants" className="slider-image" />
+            <img src="/images/dcngo.jpg" alt="DC NGO" className="slider-image" />
             <div className="text-overlay">
-              <h1>Virgiways Consultants</h1>
+              <h1>DC NGO</h1>
               <button>View website</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slider-item">
-            <img src="/images/ngoconnect.jpg" alt="Virgiways Consultants" className="slider-image" />
+            <img src="/images/ngoconnect.jpg" alt="NGO Connect" className="slider-image" />
             <div className="text-overlay">
-              <h1>Virgiways Consultants</h1>
+              <h1>NGO Connect</h1>
               <button>View website</button>
             </div>
           </div>
         </SwiperSlide>
       </Swiper>
 
-      {/* Second Swiper with 4 cards */}
+      {/* Second Swiper with 4 cards (Left-to-Right - Reverse) */}
       <Swiper
         spaceBetween={20}
         slidesPerView={3}
-        
         loop={true}
         modules={[Autoplay]}
-        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+          reverseDirection: true, // Reverse sliding direction
+        }}
         className="swiper-container"
       >
         <SwiperSlide>
           <div className="slider-item">
-            <img src="/images/PAWAIT.jpg" alt="Virgiways Consultants" className="slider-image" />
+            <img src="/images/PAWAIT.jpg" alt="PAWAIT" className="slider-image" />
             <div className="text-overlay">
-              <h1>Virgiways Consultants</h1>
+              <h1>PAWAIT</h1>
               <button>View website</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slider-item">
-            <img src="/images/REALESTATE.jpg" alt="Virgiways Consultants" className="slider-image" />
+            <img src="/images/REALESTATE.jpg" alt="Real Estate" className="slider-image" />
             <div className="text-overlay">
-              <h1>Virgiways Consultants</h1>
+              <h1>Real Estate</h1>
               <button>View website</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slider-item">
-            <img src="/images/ROYALTY.jpg" alt="Virgiways Consultants" className="slider-image" />
+            <img src="/images/ROYALTY.jpg" alt="Royalty" className="slider-image" />
             <div className="text-overlay">
-              <h1>Virgiways Consultants</h1>
+              <h1>Royalty</h1>
               <button>View website</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slider-item">
-            <img src="/images/YOUTHALIVE.jpg" alt="Virgiways Consultants" className="slider-image" />
+            <img src="/images/YOUTHALIVE.jpg" alt="Youth Alive" className="slider-image" />
             <div className="text-overlay">
-              <h1>Virgiways Consultants</h1>
+              <h1>Youth Alive</h1>
               <button>View website</button>
             </div>
           </div>
