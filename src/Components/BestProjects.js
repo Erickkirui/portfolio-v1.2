2b798b95f82a265562738d45e9410956c306dab9
Erickkirui <erickkirui653@gmail.com>
2025-01-23 13:52:45 +0300

    Fix ESLint warning by removing redundant alt attribute

--- src/Components/BestProjects.js
+++ src/Components/BestProjects.js
@@ -1,28 +1,23 @@
-import React from 'react'
-import '../Styles/project.css'
+import React from 'react';
+import '../Styles/project.css';
 
 function BestProjects() {
   return (
     <div className='featured-project'>
       <h1>Featured Project</h1>
       <h2>Ngo Connect</h2>
-      <p>NGOconnect is a user - friendly platform that connects users with a comprehensive list of NGOs in Kenya, enabling you 
-        to explore missions, volunteer opportunities, and fundraising campaigns
-        </p>
-        <h3>Technoligies</h3>
-        <ul>
-            <li>
-                Flask
-            </li>
-            <li>
-                React
-            </li>
-        </ul>
-        <img  src='/images/ngoconnect.jpg' alt='image for project'/>
-
-
+      <p>
+        NGOconnect is a user-friendly platform that connects users with a comprehensive list of NGOs in Kenya, enabling you
+        to explore missions, volunteer opportunities, and fundraising campaigns.
+      </p>
+      <h3>Technologies</h3>
+      <ul>
+        <li>Flask</li>
+        <li>React</li>
+      </ul>
+      <img src='/images/ngoconnect.jpg' />
     </div>
-  )
+  );
 }
 
-export default BestProjects
+export default BestProjects;
