93f8b08428a9b37946e330a7d32c581d0880a69f
Erickkirui <erickkirui653@gmail.com>
2025-08-27 17:41:35 +0300

    form test

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import HeroSection from '../Components/HeroSection'
-import ProjectsSlider from '../Components/ProjectsSlider'
-import Technologies from '../Components/Technologies'
-import BestProjects from '../Components/BestProjects'
+import React, { useEffect } from "react";
+import HeroSection from "../Components/HeroSection";
+import ProjectsSlider from "../Components/ProjectsSlider";
+import Technologies from "../Components/Technologies";
+import BestProjects from "../Components/BestProjects";
 
 function LandingPage() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://link.streamlyne.io/js/form_embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div className='home-page-container'>
+    <div className="home-page-container">
       {/* Hero Section */}
       <section id="about-section">
         <HeroSection />
@@ -21,8 +32,28 @@ function LandingPage() {
         <BestProjects />
       </section>
 
+      {/* Embedded Form */}
+      <section>
+        <iframe
+          src="https://link.streamlyne.io/widget/form/FYvFwfItX2PIb4Bg5gWs"
+          style={{ width: "100%", height: "600px", border: "none", borderRadius: "3px" }}
+          id="inline-FYvFwfItX2PIb4Bg5gWs"
+          data-layout="{'id':'INLINE'}"
+          data-trigger-type="alwaysShow"
+          data-trigger-value=""
+          data-activation-type="alwaysActivated"
+          data-activation-value=""
+          data-deactivation-type="neverDeactivate"
+          data-deactivation-value=""
+          data-form-name="Erick Portfiolio"
+          data-height="undefined"
+          data-layout-iframe-id="inline-FYvFwfItX2PIb4Bg5gWs"
+          data-form-id="FYvFwfItX2PIb4Bg5gWs"
+          title="Erick Portfiolio"
+        />
+      </section>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
